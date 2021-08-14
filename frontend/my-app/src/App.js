cf3800f9c0f78b062732230dbdfd2919bd658539
Maksim Shuba <Vanish404@users.noreply.github.com>
2021-08-14 15:45:56 +0300

    feat: add project structure

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Provider } from "jotai";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Users from "./components/users/users";
+import User from "./components/users/user";
+
+const App = () => (
+  <Provider>
+    <BrowserRouter basename="/">
+      <Suspense fallback="loading...">
+        <Switch>
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/users/:id" component={User} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  </Provider>
+)
 
 export default App;
